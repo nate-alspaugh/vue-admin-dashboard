4ab4b8fae9be2d57e0c44c2dffb15c8067fbc644
Nathan Alspaugh <nate.alspaugh18@gmail.com>
2020-01-03 07:50:43 -0700

    Changes for styling and templates

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import About from "../views/About.vue";
+import SignIn from "../views/SignInFlow/SignIn";
+import Request from "../views/SignInFlow/Request";
+import Recover from "../views/SignInFlow/Recover";
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,14 +14,26 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: SignIn
+  },
+  {
+    path: '/request',
+    name: 'request',
+    component: Request
+  },
+  {
+    path: 'recover',
+    name: 'recover',
+    component: Recover
   }
+
 ];
 
 const router = new VueRouter({
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "SignIn"
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+}
+</style>
